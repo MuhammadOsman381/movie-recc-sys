3cf1be56a793417e6cbe00d56f6f64e687a5a77c
Muhammad Osman <mosman257@gmail.com>
2026-04-27 13:32:03 +0500

    ---

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, {  useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import {  LogOut, Clapperboard, Bell } from 'lucide-react';
+import {  LogOut, Clapperboard } from 'lucide-react';
 import { useAuthStore } from '../store/useAuthStore';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
